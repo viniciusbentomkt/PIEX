eaaf2eeb375c6e684ab20cb8e24109a5d7103bb9
viniciusbentomkt <viniciusbento.mkt@gmail.com>
2025-11-07 18:08:37 -0300

    Exclusão de arquivos desnecessários
    Exclusão de arquivos desnecessários

--- src_pages_contact_Version2.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <main style={{ padding: 32 }}>
-      <h1>Contato</h1>
-      <p>Coloque aqui um formulário (ex.: Formspree), e-mail ou telefone.</p>
-    </main>
-  );
-}
